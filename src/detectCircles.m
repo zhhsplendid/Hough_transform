af5ce6ad598644266f028bbacb23e51efae51285
zhhsplendid <zhhy409923957@163.com>
2015-10-09 20:47:55 -0500

    really finished!!!!

--- src/detectCircles.m
+++ src/detectCircles.m
@@ -1,4 +1,16 @@
 function [centers] = detectCircles(im, radius, usegradient)
-    [centers, radius] = detectCirclesAnyRadius( im, fixRadius, usegradient);
+% Hough Transform circle detector that takes an input
+% image and a fixed radius, and returns the centers of any detected
+% circles of about that size.
+%@para
+% im is the input image, radius specifies the size of circle we are looking for, and
+% usegradient is a flag that allows the user to optionally exploit the gradient direction measured at
+% the edge points. The output centers is an N x 2 matrix in which each row lists the x,y position of
+% a detected circle’s center.
+
+% Simply call detectCirclesAnyRadius.m with fix radius
+    
+    [centers, radiuses] = detectCirclesAnyRadius( im, radius, usegradient);
+
 end
 
