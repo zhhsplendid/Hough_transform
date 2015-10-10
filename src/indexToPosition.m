7d3e608f8c0d2cc44f6ea0ad85d0402e0c87401f
zhhsplendid <zhhy409923957@163.com>
2015-10-09 21:44:54 -0500

    This time is really final finished...

--- src/indexToPosition.m
+++ src/indexToPosition.m
@@ -5,8 +5,10 @@ function [ ret ] = indexToPosition( x )
 %    the ret'th row or ret'th column position in image, indicates the
 %    center of circle
 
-    BIN_SIZE = 2;
+% The BIN_SIZE in voteIndex.m must equal BIN_SIZE in indexToPosition.m
+
+    BIN_SIZE = 1;
     
-    ret = (x - 1)* BIN_SIZE + floor(BIN_SIZE / 2);
+    ret = (x - 1)* BIN_SIZE + round(BIN_SIZE / 2);
 end
 
--- src/voteIndex.m
+++ src/voteIndex.m
@@ -3,7 +3,9 @@ function [ ret ] = voteIndex( x )
 % num x indicates the x'th row or x'th column position in image, return the
 % index for which vote bin x should belong to.
 
-    BIN_SIZE = 2;
+% The BIN_SIZE in voteIndex.m must equal BIN_SIZE in indexToPosition.m
+
+    BIN_SIZE = 1;
     
     ret = floor(x / BIN_SIZE) + 1;
 end
